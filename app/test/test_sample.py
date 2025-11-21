021203aa82f0d8c9e4e03d8393bf6b29860a0445
Rayo506 <167706886+Rayo506@users.noreply.github.com>
2025-11-21 17:32:12 +0100

    Update test_sample.py
    by Josep

--- app/test/test_sample.py
+++ app/test/test_sample.py
@@ -3,26 +3,22 @@ from app.main import app
 
 client = TestClient(app)
 
-# Test for POST /process
+# CAMBIAR METODOS
+# Test for POST /figuras (guarda 1)
 def test_process_data():
     response = client.post("/process", json={"value1": 10, "value2": 5})
     assert response.status_code == 200
     assert response.json() == {"result": 15}
 
-# Test for POST /process with invalid data
+# Test for POST /figuras with invalid data
 def test_process_data_invalid():
     response = client.post("/process", json={"value1": 10})
     assert response.status_code == 422
 
-# Test for GET /concat
+# Test for GET /figuras (Devuelve todas)
 def test_concatenate():
     response = client.get("/concat?param1=Hello&param2=World")
     assert response.status_code == 200
     assert response.json() == {"result": "HelloWorld"}
 
-# Test for GET /length
-def test_length_of_string():
-    response = client.get("/length?string=FastAPI")
-    assert response.status_code == 200
-    assert response.json() == {"length": 7}
 
